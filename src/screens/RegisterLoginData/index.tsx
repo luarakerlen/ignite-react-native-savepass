9f401aa990b0931d0b3c8785a406bda79e6471c5
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-01-04 23:58:57 -0300

    Adicionando novas senhas

--- src/screens/RegisterLoginData/index.tsx
+++ src/screens/RegisterLoginData/index.tsx
@@ -12,101 +12,109 @@ import { Header } from '../../components/Header';
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
 
-import {
-  Container,
-  Form
-} from './styles';
+import { Container, Form } from './styles';
 
 interface FormData {
-  service_name: string;
-  email: string;
-  password: string;
+	service_name: string;
+	email: string;
+	password: string;
 }
 
 const schema = Yup.object().shape({
-  service_name: Yup.string().required('Nome do serviço é obrigatório!'),
-  email: Yup.string().email('Não é um email válido').required('Email é obrigatório!'),
-  password: Yup.string().required('Senha é obrigatória!'),
-})
+	service_name: Yup.string().required('Nome do serviço é obrigatório!'),
+	email: Yup.string()
+		.email('Não é um email válido')
+		.required('Email é obrigatório!'),
+	password: Yup.string().required('Senha é obrigatória!'),
+});
 
 export function RegisterLoginData() {
-  const { navigate } = useNavigation();
-  const {
-    control,
-    handleSubmit,
-    formState: {
-      errors
-    }
-  } = useForm({
-    resolver: yupResolver(schema)
-  });
+	const { navigate } = useNavigation();
+	const {
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(schema),
+	});
 
-  async function handleRegister(formData: FormData) {
-    const newLoginData = {
-      id: String(uuid.v4()),
-      ...formData
-    }
+	async function handleRegister(formData: FormData) {
+		const newLoginData = {
+			id: String(uuid.v4()),
+			...formData,
+		};
 
-    const dataKey = '@savepass:logins';
+		const dataKey = '@savepass:logins';
 
-    // Save data on AsyncStorage and navigate to 'Home' screen
-  }
+		// Save data on AsyncStorage and navigate to 'Home' screen
+		try {
+			const dataStored = await AsyncStorage.getItem(dataKey);
+			const currentData = dataStored ? JSON.parse(dataStored) : [];
+			const newDataList = [...currentData, newLoginData];
+			await AsyncStorage.setItem(dataKey, JSON.stringify(newDataList));
+      
+      navigate('Home')
+		} catch (error) {
+			console.log(error);
+			Alert.alert('Não foi possível salvar');
+		}
+	}
 
-  return (
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-      enabled
-    >
-      <Header />
-      <Container>
-        <Form>
-          <Input
-            testID="service-name-input"
-            title="Nome do serviço"
-            name="service_name"
-            error={
-              // Replace here with real content
-              'Has error ? show error message'
-            }
-            control={control}
-            autoCapitalize="sentences"
-            autoCorrect
-          />
-          <Input
-            testID="email-input"
-            title="E-mail"
-            name="email"
-            error={
-              // Replace here with real content
-              'Has error ? show error message'
-            }
-            control={control}
-            autoCorrect={false}
-            autoCapitalize="none"
-            keyboardType="email-address"
-          />
-          <Input
-            testID="password-input"
-            title="Senha"
-            name="password"
-            error={
-              // Replace here with real content
-              'Has error ? show error message'
-            }
-            control={control}
-            secureTextEntry
-          />
+	return (
+		<KeyboardAvoidingView
+			style={{ flex: 1 }}
+			behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+			enabled
+		>
+			<Header />
+			<Container>
+				<Form>
+					<Input
+						testID='service-name-input'
+						title='Nome do serviço'
+						name='service_name'
+						error={
+							// Replace here with real content
+							errors.service_name && errors.service_name.message
+						}
+						control={control}
+						autoCapitalize='sentences'
+						autoCorrect
+					/>
+					<Input
+						testID='email-input'
+						title='E-mail'
+						name='email'
+						error={
+							// Replace here with real content
+							errors.email && errors.email.message
+						}
+						control={control}
+						autoCorrect={false}
+						autoCapitalize='none'
+						keyboardType='email-address'
+					/>
+					<Input
+						testID='password-input'
+						title='Senha'
+						name='password'
+						error={
+							// Replace here with real content
+							errors.password && errors.password.message
+						}
+						control={control}
+						secureTextEntry
+					/>
 
-          <Button
-            style={{
-              marginTop: RFValue(8)
-            }}
-            title="Salvar"
-            onPress={handleSubmit(handleRegister)}
-          />
-        </Form>
-      </Container>
-    </KeyboardAvoidingView>
-  )
+					<Button
+						style={{
+							marginTop: RFValue(8),
+						}}
+						title='Salvar'
+						onPress={handleSubmit(handleRegister)}
+					/>
+				</Form>
+			</Container>
+		</KeyboardAvoidingView>
+	);
 }
